616de8aa1fdcf5a12095cd6ab1b14f657e4aabfe
Darius Daniel <dariusdaniel771@gmail.com>
2023-02-22 03:14:27 +0100

    Quick sort trial

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,73 +1,58 @@
 #include "sort.h"
 
-
-void swap(int *array, unsigned int left, unsigned int right);
-void lomuto(int *array, size_t size, unsigned int low, unsigned int high);
-
 /**
- * quick_sort - sorts the elements of an array in ascending order using the
- * Quick sort algorithm that utilizes the Lomuto partition scheme
+ * quick_sort - function that sorts an array
+ * of integers in ascending order using the
+ * quick sort algorithm
  *
- * @array: the array to be sorted
- * @size: the number of elements in the array
-*/
+ * @array: input arrray
+ * @size: size of the array
+ * Return: no return
+ */
 void quick_sort(int *array, size_t size)
 {
 	lomuto(array, size, 0, size - 1);
 }
-
 /**
- * lomuto - helper for the quick_sort function. Splits the array
- * into subarray calling itself recursively.
- * @array: the array
- * @size: number of elements in @array
- * @low: index of the first element of the sublist
- * @high:  index of the last element of the sublist
-*/
-void lomuto(int *array, size_t size, unsigned int low, unsigned int high)
+ * lomuto - auxiliary function for the
+ * quick_sort function
+ * @a: input arrray
+ * @low: index for the first element
+ * @high: index for the last element
+ * @size: size of the array
+ * Return: no return
+ */
+void lomuto(int *array, size_t size, int low, int high)
 {
-	unsigned int pivot_idx, j, i;
-	int pivot;
+	int pivot_idx, j, i;
+	int tmp;
 
 	if (low < high)
 	{
-		pivot = array[high];
 		pivot_idx = high;
 		j = low;
 		for (i = low; i < high; i++)
 		{
-			if (array[i] < pivot)
+			if (array[i] < array[pivot_idx])
 			{
-				if (i != j)
+				if (i != pivot_idx)
 				{
-					swap(array, i, j);
+					tmp = array[i];
+					array[i] = array[j];
+					array[j] = tmp;
 					print_array(array, size);
 				}
 				j++;
 			}
 		}
-		if (j != pivot_idx && array[j] != pivot)
+		if (j != pivot_idx && array[j] != array[pivot_idx])
 		{
-			swap(array, j, pivot_idx);
+			tmp = array[j];
+			array[j] = array[pivot_idx];
+			array[pivot_idx] = tmp;
 			print_array(array, size);
 		}
-		lomuto(array, size, low, j);
+		lomuto(array, size, low, j - 1);
 		lomuto(array, size, j + 1, high);
 	}
-}
-
-/**
- * swap - swaps two elements in an array
- * @array: the array
- * @left: the leftmost element
- * @right: the rightmost element
- * Return: Nothing
-*/
-void swap(int *array, unsigned int left, unsigned int right)
-{
-	int temp;
-
-	temp = array[left];
-	array[left] = array[right];
-	array[right] = temp;
 }
